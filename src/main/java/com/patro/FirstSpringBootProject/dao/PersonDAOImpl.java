19d78690358c3a676b1078410b1644117e4af8bd
AnkitPatro <ankitpatro009@gmail.com>
2020-05-18 22:37:57 -0400

    Data Persisting using JDBCTemplate

--- src/main/java/com/patro/FirstSpringBootProject/dao/PersonDAOImpl.java
+++ src/main/java/com/patro/FirstSpringBootProject/dao/PersonDAOImpl.java
@@ -1,6 +1,8 @@
 package com.patro.FirstSpringBootProject.dao;
 
 import com.patro.FirstSpringBootProject.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,7 +13,13 @@ import java.util.UUID;
 @Repository("db1")
 public class PersonDAOImpl implements PersonDAO {
 
+    private final JdbcTemplate jdbcTemplate;
     private static List<Person> persons = new ArrayList<>();
+
+    @Autowired
+    public PersonDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public int addPersonWithId(UUID id, Person person) {
@@ -21,12 +29,24 @@ public class PersonDAOImpl implements PersonDAO {
 
     @Override
     public List<Person> getAllPerson() {
-        return persons;
+        String sql = "SELECT id, name from person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return persons.stream().filter(person -> person.getId().equals(id)).findFirst();
+        //     return persons.stream().filter(person -> person.getId().equals(id)).findFirst();
+        String sql = "SELECT id, name from person WHERE id = ?";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+            UUID personId = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(personId, name);
+        });
+        return Optional.ofNullable(person);
     }
 
     @Override
@@ -42,12 +62,12 @@ public class PersonDAOImpl implements PersonDAO {
     @Override
     public int updatePersonById(UUID id, Person person) {
         return selectPersonById(id).map(person1 -> {
-           int indexOfPerson = persons.indexOf(person1);
-           if(indexOfPerson >= 0 ) {
-               persons.set(indexOfPerson, new Person(id, person.getName()));
-               return 1;
-           }
-           return 0;
+            int indexOfPerson = persons.indexOf(person1);
+            if (indexOfPerson >= 0) {
+                persons.set(indexOfPerson, new Person(id, person.getName()));
+                return 1;
+            }
+            return 0;
         }).orElse(0);
     }
 }
--- src/test/java/com/patro/FirstSpringBootProject/FirstSpringBootProjectApplicationTests.java
+++ src/test/java/com/patro/FirstSpringBootProject/FirstSpringBootProjectApplicationTests.java
@@ -3,11 +3,11 @@ package com.patro.FirstSpringBootProject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FirstSpringBootProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class FirstSpringBootProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
